8bce59c43efb9498530a1ac04b5ff58110f026be
Gabriel <gabrielcpcontato@outlook.com>
2021-04-18 16:02:28 -0300

    Project created

---
+++ backend/src/test/java/com/gabriel/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
