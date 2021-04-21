a9d5efe6353fa48e6a0a851c0fd1dff58de83576
Gabriel <gabrielcpcontato@outlook.com>
2021-04-21 16:08:24 -0300

    /POST Orders

--- backend/src/main/java/com/gabriel/dsdeliver/services/ProductService.java
+++ backend/src/main/java/com/gabriel/dsdeliver/services/ProductService.java
@@ -17,10 +17,13 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	
 	@Transactional(readOnly = true)
 	public List<ProductDTO> findAll(){
 		List<Product> list = repository.findAllByOrderByNameAsc();
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
+	
+	
 
 }
